b8486ba9447afd1568c4079140e338d5d50f84dc
seanrt <seanrtohidi@gmail.com>
2016-04-02 14:49:40 -0400

    Enemies spawn now

--- PyMan.py
+++ PyMan.py
@@ -2,19 +2,25 @@ import os, sys
 import pygame
 from pygame.locals import *
 from helpers import *
+from random import randint
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
 # Main Python class
 class PyManMain:
-	def __init__(self, width=640,height=480):
+	def __init__(self, width=800,height=600):
 		# """Initialize"""
 		# """Initialize PyGame"""
 		pygame.init()
 		# """Set the window Size"""
 		self.width = width
 		self.height = height
+		# Main variables
+		self.score = 0
+		self.spawnRate = 250
+		self.enemyCount = 0
+		self.enemyMax = 10
 		# """Create the Screen"""
 		self.screen = pygame.display.set_mode((self.width, self.height))
 
@@ -31,45 +37,70 @@ class PyManMain:
 					or (event.key == K_LEFT)
 					or (event.key == K_UP)
 					or (event.key == K_DOWN)):
-						self.snake.move(event.key)
+						pass
+						# self.snake.move(event.key)
 						# """Check for collision"""
-						lstCols = pygame.sprite.spritecollide(self.snake, self.pellet_sprites, True)
+						# lstCols = pygame.sprite.spritecollide(self.snake, self.pellet_sprites, True)
 						# """Update the amount of pellets eaten"""
-						self.snake.pellets = self.snake.pellets + len(lstCols)
+						# self.snake.pellets = self.snake.pellets + len(lstCols)
 			self.screen.fill([0,0,0])
+
+			if (randint(1,self.spawnRate) == self.spawnRate) and self.enemyCount != self.enemyMax:
+				self.spawnEnemy()
+				self.enemyCount += 1
+
 			if pygame.font:
 				font = pygame.font.Font(None, 36)
-				text = font.render("Pellets %s" % self.snake.pellets, 1, (255, 0, 0))
+				text = font.render("Enemies %s" % self.enemyCount, 1, (255, 0, 0))
 				textpos = text.get_rect(centerx=self.width/2)
 				self.screen.blit(text, textpos)
-			self.pellet_sprites.draw(self.screen)
-			self.snake_sprites.draw(self.screen)
+				# self.screen.blit(font.render("Test",1,(255,255,255)),text.get_rect(centerx=w,centery=h))
+			# self.pellet_sprites.draw(self.screen)
+			# self.snake_sprites.draw(self.screen)
+			self.enemy_sprites.draw(self.screen)
+			self.base_sprites.draw(self.screen)
 			pygame.display.flip()
 			pygame.display.update()
 
 	def LoadSprites(self):
 		# """Load the sprites that we need"""
-		self.snake = Snake()
-		self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
+		# self.snake = Snake()
+		# self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
 
 		# """figure out how many pellets we can display"""
-		nNumHorizontal = int(self.width/64)
-		nNumVertical = int(self.height/64)       
+		# nNumHorizontal = int(self.width/64)
+		# nNumVertical = int(self.height/64)       
 		# """Create the Pellet group"""
-		self.pellet_sprites = pygame.sprite.Group()
-		# """Create all of the pellets and add them to the pellet_sprites group"""
-		for x in range(nNumHorizontal):
-			for y in range(nNumVertical):
-				self.pellet_sprites.add(Pellet(pygame.Rect(x*64, y*64, 64, 64)))
+		# self.pellet_sprites = pygame.sprite.Group()
+		# # """Create all of the pellets and add them to the pellet_sprites group"""
+		# for x in range(nNumHorizontal):
+		# 	for y in range(nNumVertical):
+		# 		self.pellet_sprites.add(Pellet(pygame.Rect(x*64, y*64, 64, 64)))
+
+		self.enemy_sprites = pygame.sprite.Group()
+
+		# Find the bottom left of the screen and put the base there
+		self.base = Base()
+		self.base.rect.move_ip(int(self.width*(-0.025)),int(self.height*0.925))
+		self.base_sprites = pygame.sprite.RenderPlain((self.base))
+
+	def spawnEnemy(self):
+		flag = randint(0,1)
+		if flag:	# Here the enemy spawns along the top
+			h = 0
+			w = randint(0,int(self.width*0.9))
+		else:		# Here the enemy spawns along the right
+			w = int(self.width*0.95)
+			h = randint(0,self.height)
+		self.enemy_sprites.add(Snake(pygame.Rect(w, h, w, h)))
 
 # """This is our snake that will move around the screen"""
 class Snake(pygame.sprite.Sprite):
-	def __init__(self):
+	def __init__(self, rect=None):
 		pygame.sprite.Sprite.__init__(self) 
 		self.image, self.rect = load_image('snake.png',-1)
-		self.pellets = 0
-		self.x_dist = 5
-		self.y_dist = 5
+		if rect != None:
+			self.rect = rect
 
 	def move(self, key):
 		# """Move your self in one of the 4 directions according to key"""
@@ -87,7 +118,7 @@ class Snake(pygame.sprite.Sprite):
 			yMove = self.y_dist
 		self.rect.move_ip(xMove,yMove);
 
-class Pellet(pygame.sprite.Sprite):
+class Base(pygame.sprite.Sprite):
 	def __init__(self, rect=None):
 		pygame.sprite.Sprite.__init__(self) 
 		self.image, self.rect = load_image('pellet.png',-1)
