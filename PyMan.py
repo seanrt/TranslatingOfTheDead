ff3a4584006eda85992b3801b01985c65f549557
seanrt <seanrtohidi@gmail.com>
2016-04-02 19:32:08 -0400

    The game basically works now, need to set up the word bank

--- PyMan.py
+++ PyMan.py
@@ -11,8 +11,12 @@ if not pygame.mixer: print 'Warning, sound disabled'
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
-ENEMYTEXT = ["one","two","three","four","five"]
-NUMWORDS = 5
+ENEMYTEXT = ["one","two","three","four","five","six","seven","eight","nine","ten"]
+NUMWORDS = 10
+BASESPAWNRATE = 250
+BASEMAXENEMIES = 5
+BASEMINENEMYSPEED = 15
+BASEMAXENEMYSPEED = 10
 
 # Main Python class
 class PyManMain:
@@ -25,90 +29,111 @@ class PyManMain:
 		# Set the base position
 		self.baseWidth = int(self.width*(-0.025))
 		self.baseHeight = int(self.height*0.925)
-		# Main variables
-		self.alive = 1
-		self.timer = 0
-		self.score = 0
-		self.spawnRate = 250
-		self.enemyCount = 0
-		self.enemyMax = 10
-		self.enemySpeedMin = 5
-		self.text = ''
 		# """Create the Screen"""
 		self.screen = pygame.display.set_mode((self.width, self.height))
 
 	def MainLoop(self):
-		# """Load All of our Sprites"""
-		self.LoadSprites()
+		while 1:
+			# Main variables
+			self.alive = 1
+			self.timer = 0
+			self.score = 0
+			self.level = 1
+			self.spawnRate = BASESPAWNRATE
+			self.minEnemySpeed = BASEMINENEMYSPEED
+			self.maxEnemySpeed = BASEMAXENEMYSPEED
+			self.maxEnemies = BASEMAXENEMIES
+			self.enemyCount = 0
+			self.text = ''
+			# """Load All of our Sprites"""
+			self.LoadSprites()
 
-		# This is the main game state loop
-		while self.alive:
-			self.timer += 1
-			scoreFont = pygame.font.Font(None, 36)
+			# This is the main game state loop
+			while self.alive:
+				self.timer += 1
+				scoreFont = pygame.font.Font(None, 36)
 
-			# Here we get text inputs or quit the game
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT: 
-					sys.exit()
-				elif event.type == KEYDOWN:
-					if event.unicode.isalpha():
-						self.text += event.unicode
-					elif event.key == K_BACKSPACE:
-						self.text = self.text[:-1]
-					elif event.key == K_RETURN:
-						for enemy in self.enemy_sprites:
-							if enemy.text == self.text:
-								enemy.boom()
-						self.text = ""
+				# Here we get text inputs or quit the game
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT: 
+						sys.exit()
+					elif event.type == KEYDOWN:
+						if event.unicode.isalpha():
+							self.text += event.unicode
+						elif event.key == K_BACKSPACE:
+							self.text = self.text[:-1]
+						elif event.key == K_RETURN:
+							for enemy in self.enemy_sprites:
+								if enemy.text == self.text:
+									enemy.boom()
+									self.score += 1
+							self.text = ""
 
-			# Enemies are spawned randomly, up to a certain amount. They then move
-			if self.enemyCount != self.enemyMax:
-				if randint(1,self.spawnRate) == self.spawnRate:
-					self.spawnEnemy(randint(self.enemySpeedMin,10),ENEMYTEXT[randint(0,NUMWORDS-1)])
-					self.enemyCount += 1
-			for enemy in self.enemy_sprites:
-				if (enemy.speed != 0) and (self.timer%enemy.speed == 0):
-					enemy.move(self.base.rect.left,self.base.rect.top)
-				elif enemy.speed == 0:
-					enemy.shrink()
-					if enemy.size < 10:
-						self.enemy_sprites.remove(enemy)
-			
-			# self.enemyCount = len(self.enemy_sprites)
+				# Enemies are spawned randomly, up to a certain amount. They then move
+				if self.enemyCount != self.maxEnemies:
+					if randint(1,self.spawnRate) == self.spawnRate:
+						self.spawnEnemy(randint(self.maxEnemySpeed,self.minEnemySpeed),ENEMYTEXT[randint(0,NUMWORDS-1)])
+						self.enemyCount += 1
+				for enemy in self.enemy_sprites:
+					if (enemy.speed != 0) and (self.timer%enemy.speed == 0):
+						enemy.move(self.base.rect.left,self.base.rect.top)
+					elif enemy.speed == 0:
+						enemy.shrink()
+						if enemy.size < 10:
+							self.enemy_sprites.remove(enemy)
+				
+				# self.enemyCount = len(self.enemy_sprites)
 
-			# The screen is updated every run through the loop
-			self.screen.fill([0,0,0])
-			text = scoreFont.render("Enemies %s" % self.enemyCount, 1, GREEN)
-			textpos = text.get_rect(centerx=self.width/2)
+				# The screen is updated every run through the loop
+				self.screen.fill([0,0,0])
+				text = scoreFont.render("Level: %s" % self.level, 1, GREEN)
+				textpos = text.get_rect()
+				self.screen.blit(text, textpos)
+				text = scoreFont.render("Score: %s" % self.score, 1, GREEN)
+				textpos = text.get_rect(centery=self.height*0.07)
+				self.screen.blit(text, textpos)
+				text = scoreFont.render(self.text, 1, BLUE)
+				textpos = text.get_rect(centerx=self.width/2,centery=self.height*0.95)
+				self.screen.blit(text, textpos)
+
+				for enemy in self.enemy_sprites:
+					if enemy.speed != 0:
+						text = scoreFont.render(enemy.text,1,RED)
+						textpos = text.get_rect(centerx=enemy.rect.left,centery=enemy.rect.top)
+						self.screen.blit(text,textpos)
+				self.enemy_sprites.draw(self.screen)
+				self.base_sprites.draw(self.screen)
+
+				pygame.display.flip()
+
+				# Here we move to the next level
+				if (self.enemyCount == self.maxEnemies) and (len(self.enemy_sprites) == 0):
+					self.levelUp()
+
+				# A collision ends the game
+				if pygame.sprite.spritecollide(self.base,self.enemy_sprites, True):
+					self.alive = 0
+
+			# This is for the end of the game
+			font = pygame.font.Font(None, 80)
+			text = font.render("Game Over", 1, RED)
+			textpos = text.get_rect(centerx=self.width/2,centery=self.height/2)
 			self.screen.blit(text, textpos)
-			text = scoreFont.render(self.text, 1, BLUE)
-			textpos = text.get_rect(centerx=self.width/2,centery=self.height*0.95)
+			font = pygame.font.Font(None, 36)
+			text = font.render("Press enter to play again", 1, RED)
+			textpos = text.get_rect(centerx=self.width/2,centery=self.height/2+40)
 			self.screen.blit(text, textpos)
-
-			for enemy in self.enemy_sprites:
-				if enemy.speed != 0:
-					text = scoreFont.render(enemy.text,1,RED)
-					textpos = text.get_rect(centerx=enemy.rect.left,centery=enemy.rect.top)
-					self.screen.blit(text,textpos)
-			self.enemy_sprites.draw(self.screen)
-			self.base_sprites.draw(self.screen)
-
 			pygame.display.flip()
 
-			# A collision ends the game
-			if pygame.sprite.spritecollide(self.base,self.enemy_sprites, True):
-				self.alive = 0
+			# We restart the game if enter is pressed
+			while self.alive == 0:
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT: 
+						sys.exit()
+					elif event.type == KEYDOWN:
+						if event.key == K_RETURN:
+							self.alive = 1
 
-		# This is for the end of the game
-		font = pygame.font.Font(None, 80)
-		text = font.render("Game Over", 1, RED)
-		textpos = text.get_rect(centerx=self.width/2,centery=self.height/2)
-		self.screen.blit(text, textpos)
-		pygame.display.flip()
-		while 1:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT: 
-					sys.exit()
 
 	def LoadSprites(self):
 		self.enemy_sprites = pygame.sprite.Group()
@@ -122,11 +147,19 @@ class PyManMain:
 		flag = randint(0,1)
 		if flag:	# Here the enemy spawns along the top
 			h = 0
-			w = randint(0,int(self.width*0.9))
+			w = randint(int(self.width*0.1),int(self.width*0.9))
 		else:		# Here the enemy spawns along the right
 			w = int(self.width*0.95)
 			h = randint(0,self.height)
 		self.enemy_sprites.add(Enemy(speed,text,pygame.Rect(w, h, w, h)))
+
+	def levelUp(self):
+		self.level += 1
+		self.enemyCount = 0
+		self.maxEnemies += 3
+		self.maxEnemySpeed = max(1,self.maxEnemySpeed-3)
+		self.minEnemySpeed = max(5,self.minEnemySpeed-1)
+		self.spawnRate = max(50,int(self.spawnRate*0.8))
 
 class Enemy(pygame.sprite.Sprite):
 	def __init__(self, speed, text, rect=None):
